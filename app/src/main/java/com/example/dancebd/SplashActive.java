0415faa717cb455dfb3a73979d5543e31021a3e3
Parvez Islam <bse.cse.parvze@gmail.com>
2023-07-24 13:37:35 +0600

    firstv commit

---
+++ app/src/main/java/com/example/dancebd/SplashActive.java
@@ -0,0 +1,46 @@
+package com.example.dancebd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class SplashActive extends AppCompatActivity {
+    ProgressBar splashProgress;
+    int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_active);
+        splashProgress = findViewById(R.id.splashProgress);
+        playProgress();
+
+        //Code to start timer and take action after the timer ends
+        new Handler(getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+              // Intent mySuperIntent = new Intent(SplashActive.this, ActivityHome.class);
+              //  startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
+    }
+
+    //Method to run progress bar for 5 seconds
+    private void playProgress() {
+        ObjectAnimator.ofInt(splashProgress, "progress", 100)
+                .setDuration(5000)
+                .start();
+
+    }
+}
